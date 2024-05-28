603e98624187acdccdd35bd7cec8b9adc9043986
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-05-28 23:40:26 +0300

    Fixed some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,15 @@ const newsSwiper = new Swiper(".news__cards", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
   },
 });
 
