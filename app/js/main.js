4d6d0e1e1519d916d57a8afb1f57dd9d907697dd
Yuliia Shevchenko <133769004+j-sheva@users.noreply.github.com>
2024-07-10 12:24:35 +0200

    mob and tab versions of burger menu are fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,35 @@
-document
-  .querySelector(".header__burger")
-  .addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.querySelector(".header__nav").classList.toggle("open");
-    document.querySelector(".header__social").classList.toggle("open");
+document.addEventListener('DOMContentLoaded', () => {
+  const headerBurger = document.querySelector('.header__burger');
+  const headerNav = document.querySelector('.header__nav');
+  const headerSocial = document.querySelector('.header__social');
+  const body = document.body;
+
+  headerBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('active');
+    headerNav.classList.toggle('open');
+    headerSocial.classList.toggle('open');
+    body.classList.toggle('no-scroll');
   });
 
-const newsSwiper = new Swiper(".news__cards", {
-  slidesPerView: "auto",
+  headerNav.addEventListener('click', () => {
+    headerBurger.classList.remove('active');
+    headerNav.classList.remove('open');
+    headerSocial.classList.remove('open');
+    body.classList.remove('no-scroll');
+  });
+});
+
+const newsSwiper = new Swiper('.news__cards', {
+  slidesPerView: 'auto',
   centeredSlides: true,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   autoplay: {
     delay: 4000,
@@ -24,28 +37,28 @@ const newsSwiper = new Swiper(".news__cards", {
   },
 });
 
-const popup = document.querySelector(".gallery__popup");
-const popupImage = popup.querySelector(".gallery__popup-img");
-const popupClose = popup.querySelector(".gallery__popup-close");
-const galleryItems = document.querySelectorAll(".gallery__item");
+const popup = document.querySelector('.gallery__popup');
+const popupImage = popup.querySelector('.gallery__popup-img');
+const popupClose = popup.querySelector('.gallery__popup-close');
+const galleryItems = document.querySelectorAll('.gallery__item');
 
 galleryItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    popupImage.src = item.querySelector("img").src;
-    popupImage.alt = item.querySelector("img").alt;
-    popup.style.display = "flex";
-    popup.classList.add("open");
+  item.addEventListener('click', () => {
+    popupImage.src = item.querySelector('img').src;
+    popupImage.alt = item.querySelector('img').alt;
+    popup.style.display = 'flex';
+    popup.classList.add('open');
   });
 });
 
-popupClose.addEventListener("click", () => {
-  popup.style.display = "none";
-  popup.classList.remove("open");
+popupClose.addEventListener('click', () => {
+  popup.style.display = 'none';
+  popup.classList.remove('open');
 });
 
-popup.addEventListener("click", (event) => {
+popup.addEventListener('click', (event) => {
   if (event.target !== popupImage) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
-  popup.classList.remove("open");
+  popup.classList.remove('open');
 });
